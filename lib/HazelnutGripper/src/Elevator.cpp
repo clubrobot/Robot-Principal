2bb0a6e307d2b001af5dee14c52d9022a8e64848
Awing Ding <awingding.win@gmail.com>
2026-04-29 16:07:33 +0200

    convert class to namespace

--- lib/HazelnutGripper/src/Elevator.cpp
+++ lib/HazelnutGripper/src/Elevator.cpp
@@ -7,11 +7,6 @@
 namespace HazelnutGripper
 {
 
-    AbstractMotor* Elevator::m_motor = nullptr;
-    AbstractAbsoluteEncoder* Elevator::m_encoder = nullptr;
-    float Elevator::m_angle = 0.0f;
-    PID* Elevator::m_pid = nullptr;
-
     void Elevator::init(AbstractMotor* motor, AbstractAbsoluteEncoder* encoder)
     {
         m_motor = motor;
@@ -23,7 +18,7 @@ namespace HazelnutGripper
     }
 
 
-    void Elevator::task(void *pvParameters)
+    [[noreturn]] void Elevator::task(void *pvParameters)
     {
         // Initialisation du temps pour le calcul du timestep
         uint32_t lastTime = millis();
