e07607a0233e3a42460d3173189d4fdcb5bf27b5
Awing Ding <awingding.win@gmail.com>
2026-05-02 14:02:11 +0200

    use PC constant + linear correction on BasicTurn

--- lib/BasicMoveStrategy/BasicMoveStrategy.cpp
+++ lib/BasicMoveStrategy/BasicMoveStrategy.cpp
@@ -2,35 +2,28 @@
 // Created by awing on 21/11/25.
 //
 
+#include "mathutils.h"
 #include "BasicMoveStrategy.h"
 
-float angle_init = 0;
 
 void BasicMoveStrategy::computeVelSetpoints(float timestep) {
-    float dx = getPosSetpoint().x - getPosInput().x;
-    float obj;
-    if (dx > 0 && dx > x_slowing_distance) {
-        obj = x_max_speed;  // 
-    } else if (dx < 0 && fabs(dx) > x_slowing_distance) {
-        obj = -x_max_speed;
-    } else {
-        obj = x_max_speed * fabs(dx) / x_slowing_distance;
-        obj = (dx > 0) ? obj : -obj;
-    }
-    float correction_angle = -anglToVitAngl(timestep);
-    setVelSetpoints(obj, correction_angle); //vitesse roue, et vitesse angulaire
+    const Position setPoint = getPosSetpoint();
+    const Position pos = getPosInput();
+    const float dtheta = setPoint.theta - pos.theta;
+    const float dx = setPoint.x - pos.x;
+    const float dy = setPoint.y - pos.y;
+    const float drho = dx * cos(theta_init) + dy * sin(theta_init);
+    const float linObj = fabs(drho) > getLinPosThreshold() ? saturate(getLinVelKp() * drho, -getLinVelMax(), getLinVelMax()) : 0;
+    const float angObj = fabs(dtheta) > getAngPosThreshold() ? saturate(getAngVelKp() * dtheta, -getAngVelMax(), getAngVelMax()) : 0;
+    setVelSetpoints(linObj, angObj); //vitesse roue, et vitesse angulaire
 }
 
 bool BasicMoveStrategy::getPositionReached() {
-    x_goalReached = fabs(getPosSetpoint().x - getPosInput().x) < x_precision;
-    if (x_goalReached) {
-        //printf("%f %f %f\n", getPosInput().x, getPosInput().y, getPosInput().theta);
-        //printf("L'objectif a été atteint, BasicMove::getPositionReached est ok\n");
-    }
-    return x_goalReached;
-}
-
-float BasicMoveStrategy::anglToVitAngl(float timestep) {
-    float angle_now = getPosInput().theta;
-    return (angle_now - this->theta_init) / timestep;
+    const Position setPoint = getPosSetpoint();
+    const Position pos = getPosInput();
+    const float dtheta = setPoint.theta - pos.theta;
+    const float dx = setPoint.x - pos.x;
+    const float dy = setPoint.y - pos.y;
+    const float drho = dx * cos(theta_init) + dy * sin(theta_init);
+    return fabs(drho) < getLinPosThreshold() && fabs(dtheta) < getAngPosThreshold();
 }
--- lib/BasicTurnStrategy/BasicTurnStrategy.cpp
+++ lib/BasicTurnStrategy/BasicTurnStrategy.cpp
@@ -8,23 +8,21 @@
 #include "Teleplot.h"
 
 void BasicTurnStrategy::computeVelSetpoints(float timestep) {
-    float dang = getPosSetpoint().theta - getPosInput().theta;
-    teleplot.add_variable_float_2decimal("dang", dang);
-    float obj;
-    float kp = 1;
-    if (fabs(dang) > ang_precision) {
-        obj = saturate(kp * dang, -ang_max_speed, ang_max_speed);
-    } else {
-        obj = 0;
-    }
-    setVelSetpoints(0, -obj);
+    const Position SetPoint = getPosSetpoint();
+    const Position pos = getPosInput();
+    const float dang = SetPoint.theta - pos.theta;
+    const float dx = SetPoint.x - pos.x;
+    const float dy = SetPoint.y - pos.y;
+    const float drho = dx * cos(pos.theta) + dy * sin(pos.theta);
+    const float angObj = fabs(dang) > getAngPosThreshold() ? saturate(getAngVelKp() * dang, -getAngVelMax(), getAngVelMax()) : 0;
+    const float linObj = fabs(drho) > getLinPosThreshold() ? saturate(getLinVelKp() * drho, -getLinVelMax(), getLinVelMax()) : 0;
+    setVelSetpoints(-linObj, -angObj);
 }
 
 bool BasicTurnStrategy::getPositionReached() {
-    ang_goalReached = fabs(getPosSetpoint().theta - getPosInput().theta) < ang_precision;
-    if (ang_goalReached) {
-        printf("%f %f %f\n", getPosInput().x, getPosInput().y, getPosInput().theta);
-        printf("L'objectif a été atteint, BasicTurn::getPositionReached est ok\n");
-    }
-    return ang_goalReached;
+    const Position pos = getPosInput();
+    const Position setPoint = getPosSetpoint();
+    return fabs(setPoint.theta - pos.theta) < getAngPosThreshold() &&
+           (setPoint.x - pos.x) * cos(pos.theta) + (setPoint.y - pos.y) * sin(pos.theta) < getLinPosThreshold()
+        ;
 }
